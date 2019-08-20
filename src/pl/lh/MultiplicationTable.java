9ceebe2a28be2a68f138f74071c0964cfdbd39c2
lck <lck0405@o2.pl>
2019-08-20 20:31:54 +0200

    Person, class vs instance

---
+++ src/pl/lh/MultiplicationTable.java
@@ -0,0 +1,30 @@
+package pl.lh;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class MultiplicationTable {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Program count multiplication table 1-10 for Your number" +
+                "\n Please enter Your number:  ");
+        int n = scanner.nextInt();
+        int result;
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        for (int i = 1; i<=10; i++){
+           result = n * i;
+            System.out.println(n + " x " + i + " = " + result);
+        }
+
+        scanner.close();
+    }
+}
